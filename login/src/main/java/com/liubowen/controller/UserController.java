1cfbe35116b5029f42fb3a2307ff82f29d4a1501
liubowen <liubowen>
2018-02-04 18:44:34 +0800

    Initial commit

---
+++ login/src/main/java/com/liubowen/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.liubowen.controller;
+
+import com.google.common.collect.Maps;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+/**
+ * @author liubowen
+ * @date 2018/2/4 18:22
+ * @description
+ */
+@RestController("userController")
+public class UserController {
+
+    @PostMapping("/login")
+    public Map<String, Object> login() {
+        Map<String, Object> map = Maps.newHashMap();
+        map.put("code", 0);
+        map.put("msg", "登陆成功");
+        return map;
+    }
+}
