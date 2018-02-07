bad2456d3164948d606abb110932aab763e69201
liubowen <liubowen>
2018-02-07 10:19:33 +0800

    Merge branch 'master' of https://github.com/MoringSatr/gameDemo01server
    # Conflicts:
    #	proto/src/main/java/com/liubowen/game/GameProto.java

--- core/src/main/java/com/liubowen/App.java
+++ core/src/main/java/com/liubowen/App.java
@@ -1,18 +1,24 @@
 package com.liubowen;
 
+import com.google.common.collect.Lists;
 import com.liubowen.common.Gender;
 import com.liubowen.user.entity.UserInfo;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 /**
  * Hello world!
  */
 public class App {
+
     public static void main(String[] args) {
         UserInfo userInfo1 = new UserInfo(1, "11", 10, Gender.MAN);
         UserInfo userInfo2 = new UserInfo(2, "22", 20, Gender.UNKNOW);
         UserInfo userInfo3 = new UserInfo(3, "33", 30, Gender.WOMAN);
-        List<UserInfo> userInfos ;
-
+        List<UserInfo> userInfos = Lists.newArrayList(userInfo1, userInfo2, userInfo3);
+        userInfos.sort(Comparator.comparingInt(UserInfo::getLevel));
+        userInfos.sort(Comparator.comparingLong(UserInfo::getUserId));
+        System.err.println(Arrays.toString(userInfos.toArray()));
     }
 }
